858181fc021230233459ce4e7d67f0fd705d71ac
VerifryingUname <kingdagone1@gmail.com>
2025-01-31 14:18:20 +0700

    lab15_4.cpp completed

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,19 @@ int main(){
 }
 
 //Do not modify source code above this line
+void shuffle(int &x, int &y, int &z, int &w) {
+    int ID[] = {0, 1, 2, 3};
+    int Red_envelope[] = {x, y, z, w};
+    int shuffled[4];
+    for (int i = 0; i < 4; ++i) {
+        int random = rand() % (4 - i);
+        shuffled[i] = Red_envelope[ID[random]];
+        for (int j = random; j < 3 - i; ++j) {
+            ID[j] = ID[j + 1];
+        }
+    }
+    x = shuffled[0];
+    y = shuffled[1];
+    z = shuffled[2];
+    w = shuffled[3];
+}
